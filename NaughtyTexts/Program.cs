63d55967a12d20cacae6832bb31ca4b823b1b5d3
michael lindeman <mi.lindeman@bellt.nl>
2023-05-20 22:39:55 +0200

    update 5

--- NaughtyTexts/Program.cs
+++ NaughtyTexts/Program.cs
@@ -2,35 +2,43 @@
 Console.WriteLine("How may strings are nice and not naughty");
 
 var lines = File.ReadAllLines("./input.txt").ToList();
-//var lines = new List<string>() { "^v^v^v^v^v" };
+//var lines = new List<string>() { "ugknbfddgicrmopn" };
 
 var niceCount = 0;
 var naughtyCount = 0;
+var old = true;
 
 foreach (var line in lines)
 {
     int vowel_count = 0;
     int count_double = 0;
 
-    if (line.Contains("ab") || line.Contains("cd") || line.Contains("pq") || line.Contains("xy"))
+    if (old)
     {
-        naughtyCount++;
-        continue;
+        if (line.Contains("ab") || line.Contains("cd") || line.Contains("pq") || line.Contains("xy"))
+        {
+            naughtyCount++;
+            continue;
+        }
+
+        for (int i = 0; i < line.Length; i++)
+        {
+            if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' || line[i] == 'u' || line[i] == 'A' || line[i] == 'E' || line[i] == 'I' || line[i] == 'O' || line[i] == 'U')
+                vowel_count++;
+
+            if (line.Length > i + 1 && line[i] == line[i + 1])
+                count_double++;
+        }
+
+        if (vowel_count > 2 && count_double > 0)
+            niceCount++;
+        else
+            naughtyCount++; 
     }
-
-    for (int i =0; i < line.Length; i++)
-    {
-        if (line[i] == 'a' || line[i] == 'e' || line[i] == 'i' || line[i] == 'o' || line[i] == 'u' || line[i] == 'A' || line[i] == 'E' || line[i] == 'I' || line[i] == 'O' || line[i] == 'U')
-            vowel_count++;
-
-        if (line[i] == line[i+1])
-            count_double++;
-    }
-
-    if(vowel_count>3 && count_double >1)
-        niceCount++;
     else
-        naughtyCount++;
+    {
+
+    }
 
 }
 
